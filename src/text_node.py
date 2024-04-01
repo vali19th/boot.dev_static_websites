8d680eba41defff62e848d2fd326746be21558e1
wip <vali19th@protonmail.com>
2024-04-01 10:20:14 +0300

    feat: convert text to TextNode objects

--- src/text_node.py
+++ src/text_node.py
@@ -1,3 +1,5 @@
+import re
+
 from enum import Enum
 
 from html_node import LeafNode
@@ -45,5 +47,98 @@ class TextNode:
             raise ValueError(f"Unknown text type: {text_type}")
 
 
-# def split_nodes_delimiter(old_nodes, delimiter, text_type):
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if not isinstance(node, TextNode):
+            new_nodes.append(node)
+            continue
+
+        if delimiter not in node.text:
+            new_nodes.append(node)
+            continue
+
+        splits = node.text.split(delimiter)
+        if len(splits) % 2 == 0:
+            raise ValueError(f"Delimiter {delimiter!r} not closed in {node.text!r}")
+
+        inner = node.text.index(delimiter) == 0
+        for split in splits:
+            tt = text_type if inner else node.text_type
+            new_nodes.append(TextNode(split, tt))
+            inner = not inner
+
+    return new_nodes
+
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+
+
+def extract_markdown_links(text):
+    return re.findall(r"\[(.*?)\]\((.*?)\)", text)
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if not isinstance(node, TextNode):
+            new_nodes.append(node)
+            continue
+
+        links = extract_markdown_links(node.text)
+        if not links:
+            new_nodes.append(node)
+            continue
+
+        text = node.text
+        for link in links:
+            md_link = f"[{link[0]}]({link[1]})"
+            before, _, text = text.rpartition(md_link)
+            if before:
+                new_nodes.append(TextNode(before, node.text_type))
+
+            new_nodes.append(TextNode(link[0], TT.LINK, link[1]))
+
+        if text:
+            new_nodes.append(TextNode(text, node.text_type))
+
+    return new_nodes
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if not isinstance(node, TextNode):
+            new_nodes.append(node)
+            continue
+
+        images = extract_markdown_images(node.text)
+        if not images:
+            new_nodes.append(node)
+            continue
+
+        text = node.text
+        for image in images:
+            md_image = f"![{image[0]}]({image[1]})"
+            before, _, text = text.rpartition(md_image)
+            if before:
+                new_nodes.append(TextNode(before, node.text_type))
+
+            new_nodes.append(TextNode(image[0], TT.IMAGE, image[1]))
+
+        if text:
+            new_nodes.append(TextNode(text, node.text_type))
+
+    return new_nodes
+
+
+def text_to_text_nodes(text):
+    nodes = [TextNode(text, TT.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TT.BOLD)
+    nodes = split_nodes_delimiter(nodes, "*", TT.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TT.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
 
--- tests/test_text_node.py
+++ tests/test_text_node.py
@@ -1,4 +1,14 @@
-from src.text_node import TextNode, TT
+from pprint import pprint
+from src.text_node import (
+    TextNode,
+    TT,
+    extract_markdown_images,
+    extract_markdown_links,
+    split_nodes_delimiter,
+    split_nodes_image,
+    split_nodes_link,
+    text_to_text_nodes,
+)
 
 
 def test_equality():
@@ -30,4 +40,82 @@ def test_to_html_node():
 
     n = TextNode("Twitter", TT.IMAGE, "http://www.x.com").to_html_node()
     assert n.to_html() == '<img src="http://www.x.com" alt="Twitter" />'
+
+
+def test_md_to_text_node():
+    node = TextNode("This is text with a **bolded** word", TT.TEXT)
+    assert split_nodes_delimiter([node], "**", TT.BOLD) == [
+        TextNode("This is text with a ", TT.TEXT),
+        TextNode("bolded", TT.BOLD),
+        TextNode(" word", TT.TEXT),
+    ]
+
+    node = TextNode("This is text with a `code block` word", TT.TEXT)
+    assert split_nodes_delimiter([node], "`", TT.CODE) == [
+        TextNode("This is text with a ", TT.TEXT),
+        TextNode("code block", TT.CODE),
+        TextNode(" word", TT.TEXT),
+    ]
+
+
+def test_extract_markdown_images():
+    text = "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and ![another](https://i.imgur.com/dfsdkjfd.png)"
+    assert extract_markdown_images(text) == [
+        ("image", "https://i.imgur.com/zjjcJKZ.png"),
+        ("another", "https://i.imgur.com/dfsdkjfd.png"),
+    ]
+
+
+def test_extract_markdown_links():
+    text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
+    assert extract_markdown_links(text) == [
+        ("link", "https://www.example.com"),
+        ("another", "https://www.example.com/another"),
+    ]
+
+
+def test_split_nodes_image():
+    node = TextNode(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png).",
+        TT.TEXT,
+    )
+    pprint(split_nodes_image([node]))
+    assert split_nodes_image([node]) == [
+        TextNode("This is text with an ", TT.TEXT),
+        TextNode("image", TT.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+        TextNode(" and another ", TT.TEXT),
+        TextNode("second image", TT.IMAGE, "https://i.imgur.com/3elNhQu.png"),
+        TextNode(".", TT.TEXT),
+    ]
+
+
+def test_split_nodes_link():
+    node = TextNode(
+        "This is text with an [link](https://x.com/) and another [second link](https://youtube.com/).",
+        TT.TEXT,
+    )
+    pprint(split_nodes_link([node]))
+    assert split_nodes_link([node]) == [
+        TextNode("This is text with an ", TT.TEXT),
+        TextNode("link", TT.LINK, "https://x.com/"),
+        TextNode(" and another ", TT.TEXT),
+        TextNode("second link", TT.LINK, "https://youtube.com/"),
+        TextNode(".", TT.TEXT),
+    ]
+
+
+def test_text_to_text_nodes():
+    text = "This is **text** with an *italic* word and a `code block` and an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
+    assert text_to_text_nodes(text) == [
+        TextNode("This is ", TT.TEXT),
+        TextNode("text", TT.BOLD),
+        TextNode(" with an ", TT.TEXT),
+        TextNode("italic", TT.ITALIC),
+        TextNode(" word and a ", TT.TEXT),
+        TextNode("code block", TT.CODE),
+        TextNode(" and an ", TT.TEXT),
+        TextNode("image", TT.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+        TextNode(" and a ", TT.TEXT),
+        TextNode("link", TT.LINK, "https://boot.dev"),
+    ]
 

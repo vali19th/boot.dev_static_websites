3727f40a0bbd7fb529621f2d49cc70d3814ca4c3
wip <vali19th@protonmail.com>
2024-04-03 09:03:03 +0300

    feat: impl. copying static/ to public/

--- src/main.py
+++ src/main.py
@@ -1,12 +1,31 @@
 import sys
+import shutil
 import traceback
 
-from text_node import TextNode
+from pathlib import Path
 
 
 def main():
-    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(node)
+    copy_dir("static", "public")
+
+
+def copy_dir(src, dest):
+    dest = Path(dest)
+    if dest.exists():
+        shutil.rmtree(dest, ignore_errors=True)
+        print(f"[INFO] Deleted dir {str(dest)!r}")
+
+    dest.mkdir()
+    print(f"[INFO] Created dir {str(dest)!r}")
+
+    for p in Path(src).iterdir():
+        dest_p = dest.joinpath(p.name)
+
+        if p.is_file():
+            shutil.copyfile(p, dest_p)
+            print(f"[INFO] Copied {str(p)!r} to {str(dest_p)!r}")
+        else:
+            copy_dir(p, dest_p)
 
 
 if __name__ == "__main__":
